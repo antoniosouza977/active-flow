193a20e0070cb3d5237e24a2d671786d7f6cce08
Antonio Souza <129562096+antoniosouza977@users.noreply.github.com>
2025-07-26 15:29:05 -0400

    feat: adicionar rotas e controlador para gestão de perfis (roles)

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\RolesController;
 use App\Http\Controllers\UsersController;
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
@@ -13,9 +14,13 @@ Route::get('dashboard', function () {
 })->middleware(['auth', 'verified'])->name('dashboard');
 
 Route::group(['middleware' => ['auth', 'verified']], function () {
+    // Users
     Route::resource('users', UsersController::class)->except(['show', 'destroy']);
     Route::patch('users/{user}/inactivate', [UsersController::class, 'inactivate'])->name('users.inactivate');
     Route::patch('users/{user}/activate', [UsersController::class, 'activate'])->name('users.activate');
+
+    // Roles
+    Route::resource('/roles', RolesController::class)->except('show');
 });
 
 require __DIR__.'/settings.php';
