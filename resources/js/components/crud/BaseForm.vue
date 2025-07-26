1f2021fb84ee725d6d078155fe97dae7e91e4f22
Antonio Souza <129562096+antoniosouza977@users.noreply.github.com>
2025-07-26 15:28:23 -0400

    feat: adicionar lógica reutilizável de formulário no componente BaseForm

--- resources/js/components/crud/BaseForm.vue
+++ resources/js/components/crud/BaseForm.vue
@@ -1,12 +1,47 @@
-<script setup lang="ts">
+<script setup>
 import {Button} from '@/components/ui/button'
-import {Link} from '@inertiajs/vue3'
+import {Link, router} from '@inertiajs/vue3'
+import {toast} from "vue-sonner";
+import {useForm} from "vee-validate";
+import {computed} from "vue";
 
-defineProps({
-    indexRoute: {
+const props = defineProps({
+    formSchema: {
+        required: true
+    },
+    initialValues: {
+        type: Object,
+        required: true
+    },
+    resourceLabel: {
         type: String,
-        required: true,
+        required: true
+    },
+    routePrefix: {
+        type: String,
+        required: true
     }
+})
+
+const id = computed(() => props.initialValues?.id)
+
+const {handleSubmit, setErrors} = useForm({
+    validationSchema: props.formSchema,
+    initialValues: props.initialValues
+})
+
+const submitForm = handleSubmit((values) => {
+    const options = {
+        onError: (err) => setErrors(err),
+        onSuccess: () => toast(`${props.resourceLabel} ${id.value ? 'atualizado' : 'criado'} com sucesso!`),
+        preserveScroll: true,
+    };
+
+    if (id.value) {
+        return router.patch(route(`${props.routePrefix}.update`, id.value), values, options);
+    }
+
+    return router.post(route(`${props.routePrefix}.store`), values, options);
 })
 
 </script>
@@ -15,12 +50,15 @@ defineProps({
     <div class="h-full p-4 ">
         <div class="h-full rounded-xl border border-sidebar-border/70 dark:border-sidebar-border">
             <div class="flex justify-end p-4">
-                <Link :href="indexRoute">
+                <Link :href="route(`${routePrefix}.index`)">
                     <Button>Voltar</Button>
                 </Link>
             </div>
             <div class="p-4">
-                <slot name="content" />
+                <form @submit="submitForm" class="lg:w-2/3 w-full space-y-6">
+                    <slot name="content" />
+                    <Button type="submit">Salvar</Button>
+                </form>
             </div>
 
         </div>
